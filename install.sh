bde7ec14f7de7aa2231ec46ac0f4624e15fa6832
John Letey <johnletey@gmail.com>
2020-07-05 10:07:42 +0100

    rust

---
+++ install.sh
@@ -0,0 +1,3 @@
+# this is temporary, just to list install scripts i've run
+# until i come up with a more permanent way of doing this
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -4,3 +4,4 @@ antibody bundle < ~/.dotfiles/zsh/plugins.txt
 export TYPEWRITTEN_PROMPT_LAYOUT="half_pure"
 
 source ~/.dotfiles/zsh/alias.zsh
+source ~/.dotfiles/zsh/env.zsh
